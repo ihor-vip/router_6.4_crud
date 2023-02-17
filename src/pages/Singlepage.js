99334996f92eac5676f708b17177930fbed9ca5d
ihor-vip <vip20140902@gmail.com>
2023-02-17 11:50:35 +0200

    fetching post after clicking on specific post in posts list

--- src/pages/Singlepage.js
+++ src/pages/Singlepage.js
@@ -1,11 +1,25 @@
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 export default function Singlepage() {
     const {id} = useParams();
+    const [post, setPost] = useState(null)
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(res => res.json())
+            .then(data => setPost(data))
+    },[id])
+
     return (
         <div>
             {
-
+                post && (
+                    <>
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                    </>
+                )
             }
         </div>
     );
