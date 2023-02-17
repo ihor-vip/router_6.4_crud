c2808609b8bd2d8f9cbe432783a3abac8be10d77
ihor-vip <vip20140902@gmail.com>
2023-02-17 16:37:39 +0200

    add value to AuthProvider.js

--- src/hoc/AuthProvider.js
+++ src/hoc/AuthProvider.js
@@ -1,9 +1,23 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
 
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({children}) => {
-    return <AuthContext.Provider>
+    const [user,setUser] = useState(null);
+
+    const signin = (newUser, cb) => {
+        setUser(newUser);
+        cb();
+    }
+
+    const signout = () => {
+        setUser(null)
+        cb();
+    }
+
+    const value = {user, signin, signout}
+
+    return <AuthContext.Provider value={value}>
         {children}
     </AuthContext.Provider>
 }
