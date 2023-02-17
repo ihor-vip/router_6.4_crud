16f2887e07e76d9ddea2fa4642aa6a2730b3b490
ihor-vip <vip20140902@gmail.com>
2023-02-17 10:51:50 +0200

    modify App.js with the Layout component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import {Link, Route, Routes} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Aboutpage from "./pages/Aboutpage";
 import Blogpage from "./pages/Blogpage";
 import Notfoundpage from "./pages/Notfoundpage";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <div>
       <>
           <Routes>
-              <Route path='/' element={<Homepage/>}/>
-              <Route path='/about' element={<Aboutpage/>} />
-              <Route path='/posts' element={<Blogpage/>} />
-              <Route path='*' element={<Notfoundpage/>} />
+            <Route path='/' element={<Layout/>}>
+                <Route index element={<Homepage/>}/>
+                <Route path='about' element={<Aboutpage/>} />
+                <Route path='posts' element={<Blogpage/>} />
+                <Route path='*' element={<Notfoundpage/>} />
+            </Route>
           </Routes>
       </>
     </div>
