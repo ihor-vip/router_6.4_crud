5efe69924e9b5abe13bcd0a1ac1741100b395f51
ihor-vip <vip20140902@gmail.com>
2023-02-17 11:09:56 +0200

    adding classes to NavLink

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,21 @@
 import {NavLink, Outlet} from "react-router-dom";
 
+
+const setActive = ({isActive}) => isActive ? 'active-link' : ''
 export default function Layout() {
     return (
       <>
           <header>
-              <NavLink to='/'>Home</NavLink>
-              <NavLink to='/posts'>Blog</NavLink>
-              <NavLink to='/about'>About</NavLink>
+              <NavLink to='/' className={setActive}>Home</NavLink>
+              <NavLink to='/posts' className={setActive}>Blog</NavLink>
+              <NavLink to='/about'className={setActive}>About</NavLink>
           </header>
 
           <main className='container'>
               <Outlet/>
           </main>
 
-          <footer className='container'>2023</footer>
+          <footer className='container'>&copy; ReactRouter</footer>
       </>
     );
 }
