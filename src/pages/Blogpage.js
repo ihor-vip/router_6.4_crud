4fddb8b8e1bc0ed44a48b357482a486797c33363
ihor-vip <vip20140902@gmail.com>
2023-02-17 10:00:03 +0200

    create pages for routing

---
+++ src/pages/Blogpage.js
@@ -0,0 +1,7 @@
+export default function Blogpage() {
+    return (
+        <div>
+            Blogpage
+        </div>
+    );
+}
---
+++ src/pages/Notfoundpage.js
@@ -0,0 +1,7 @@
+export default function Notfoundpage() {
+    return (
+        <div>
+            Notfoundpage
+        </div>
+    );
+}   
