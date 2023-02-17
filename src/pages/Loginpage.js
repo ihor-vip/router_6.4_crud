d568f8cdbef9fe2194e014970a94987dd2e14f87
ihor-vip <vip20140902@gmail.com>
2023-02-17 16:45:26 +0200

    add signin option to login page

--- src/pages/Loginpage.js
+++ src/pages/Loginpage.js
@@ -1,15 +1,30 @@
 import {useLocation, useNavigate} from "react-router-dom";
+import {useAuth} from "../hook/useAuth";
 
 const Loginpage = () => {
     const navigate = useNavigate();
     const location = useLocation();
+    const {signin} = useAuth();
 
     const fromPage = location.state?.from?.pathname || '/';
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const user = form.username.value;
+
+        signin(user, () => navigate(fromPage), {replace: true})
+    }
 
     return (
         <div>
             <h1>Login page</h1>
-            {fromPage}
+            <form onClick={handleSubmit}>
+                <label>
+                    Name: <input name='username'/>
+                </label>
+                <button type='submit'>Login</button>
+            </form>
         </div>
     )
 }
