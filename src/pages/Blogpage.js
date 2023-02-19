3bfc1b745a248cd0821f43df3be3c7d87bb78fea
ihor-vip <vip20140902@gmail.com>
2023-02-19 09:12:48 +0200

    adding defer functionality for displaying posts

--- src/pages/Blogpage.js
+++ src/pages/Blogpage.js
@@ -1,8 +1,9 @@
-import {Link, useLoaderData, useSearchParams} from "react-router-dom";
+import {Await, defer, Link, useLoaderData, useSearchParams} from "react-router-dom";
 import {BlogFilter} from "../components/BlogFilter";
+import {Suspense} from "react";
 
 const Blogpage = () => {
-    const posts = useLoaderData();
+    const {posts} = useLoaderData();
     const [searchParams, setSearchParams] = useSearchParams();
 
     const postQuery = searchParams.get('post') || '';
@@ -16,22 +17,39 @@ const Blogpage = () => {
             <BlogFilter postQuery={postQuery} latest={latest} setSearchParams={setSearchParams}/>
 
             <Link to='/posts/new'>Add new post</Link>
-            {
-                posts.filter(
-                    post => post.title.includes(postQuery) && post.id >= startsFrom
-                ).map(post => (
-                    <Link key={post.id} to={`/posts/${post.id}`}>
-                        <li>{post.title}</li>
-                    </Link>
-                ))
-            }
+
+            <Suspense fallback={<h2>Loading...</h2>}>
+                <Await resolve={posts}>
+                    {
+                        (resolvedPosts) => (
+                            <>
+                                {
+                                    resolvedPosts.filter(
+                                        post => post.title.includes(postQuery) && post.id >= startsFrom
+                                    ).map(post => (
+                                        <Link key={post.id} to={`/posts/${post.id}`}>
+                                            <li>{post.title}</li>
+                                        </Link>
+                                    ))
+                                }
+                            </>
+                        )
+                    }
+                </Await>
+            </Suspense>
         </div>
     );
 }
 
-const blogLoader = async () => {
+
+async function getPosts() {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
     return res.json()
+}
+const blogLoader = async () => {
+    return defer({
+        posts: getPosts()
+    })
 }
 
 export {Blogpage, blogLoader}
